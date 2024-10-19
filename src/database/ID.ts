12ceef395a1f4aac5d61b187978aaf706307bb30
MihaiZecheru <zecheruchris@gmail.com>
2024-10-18 20:53:10 -0700

    Add ID for folders and files

--- src/database/ID.ts
+++ src/database/ID.ts
@@ -4,5 +4,7 @@ type Branded<T, B> = T & Brand<B>;
 
 // ----------------------------------------
 
-type ID = Branded<`${string}-${string}-${string}-${string}`, "ID">; // UUID
+type ID = Branded<`${string}-${string}-${string}-${string}`, "ID">;
 export type UserID = Branded<ID, "UserID">;
+export type FileID = Branded<ID, "FileID">;
+export type FolderID = Branded<ID, "FolderID">;
