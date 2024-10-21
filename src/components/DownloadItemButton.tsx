8b07ae01d8b1ddaa2ed11cbfdc973af4a5bbf9a4
MihaiZecheru <zecheruchris@gmail.com>
2024-10-20 22:54:16 -0700

    Use infoModal for error handling

--- src/components/DownloadItemButton.tsx
+++ src/components/DownloadItemButton.tsx
@@ -1,27 +1,28 @@
 import { IconButton } from "@mui/material";
 import { GetFileFromGDrive } from "../database/GoogleDrive";
 import { TFile } from "../database/types";
+import useInfoModal from "./base/useInfoModal";
 
 interface Props {
   file: TFile;
 }
 
 const DownloadFileButton = ({ file }: Props) => {
-  // Using the anchor tag method
-  const downloadBlob = (blob: Blob, filename: string) => {
-    const link = document.createElement('a');
-    const url = URL.createObjectURL(blob);
-    link.href = url;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
-  };
+  const { showInfoModal } = useInfoModal();
 
   const downloadItem = async () => {
-    const blob: Blob = await GetFileFromGDrive(file.gdrive_file_id);
-    downloadBlob(blob, file.name);
+    await GetFileFromGDrive(file.gdrive_file_id).then((blob: Blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = file.name;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    }).catch((err) => {
+      console.error(err);
+      showInfoModal('Error', 'Failed to download file.');
+    });
   };
 
   return (
