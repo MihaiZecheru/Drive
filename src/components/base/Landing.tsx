f30629d94217425c0e2e30d0f07f29a9f0ae8487
MihaiZecheru <zecheruchris@gmail.com>
2024-10-23 17:32:51 -0700

    Add landing page

--- src/components/base/Landing.tsx
+++ src/components/base/Landing.tsx
@@ -1,8 +1,18 @@
+import { Button, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const Landing = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      TODO: make Landing
-    </>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#b0c4de' }}>
+      <Paper elevation={3} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="logo" width={50} style={{ margin: '1rem' }} />
+        <Button style={{ margin: '.5rem' }} color='primary' role='button' onClick={() => navigate('/login')}>
+          Get unlimited free storage with Drive
+        </Button>
+      </Paper>
+    </div>
   );
 }
  
